f332b7fc94992d68ca02b98bf84cdf3a342a0021
Jarek Jakubowski <egger1991@gmail.com>
2018-05-18 01:05:02 +0200

    Fix PostgreSQL syntax error
    And clean up code

--- src/Service/WhoIsEditingService.php
+++ src/Service/WhoIsEditingService.php
@@ -2,8 +2,7 @@
 
 namespace Bolt\Extension\TwoKings\WhoIsEditing\Service;
 
-use Bolt\Application;
-use Bolt\Users;
+use Doctrine\DBAL\Connection;
 use Symfony\Component\HttpFoundation\Request;
 
 /**
@@ -13,11 +12,10 @@ use Symfony\Component\HttpFoundation\Request;
  *
  * @author Néstor de Dios Fernández <nestor@twokings.nl>
  */
-
-class WhoIsEditingService {
-
+class WhoIsEditingService
+{
     /**
-     * @var Doctrine\DBAL\Connection
+     * @var Connection
      */
     private $database;
 
@@ -26,11 +24,10 @@ class WhoIsEditingService {
      */
     private $actionsMetaData = [
         'editcontent' => ['text' => 'is editing', 'class' => 'alert-warning'],
-        'update'      => ['text' => 'updated',    'class' => 'alert-success'],
-        'close'       => ['text' => 'closed',     'class' => 'alert-info'],
-        'delete'      => ['text' => 'deleted',    'class' => 'alert-danger'],
+        'update' => ['text' => 'updated', 'class' => 'alert-success'],
+        'close' => ['text' => 'closed', 'class' => 'alert-info'],
+        'delete' => ['text' => 'deleted', 'class' => 'alert-danger'],
     ];
-
 
     function __construct($database)
     {
@@ -38,39 +35,34 @@ class WhoIsEditingService {
     }
 
     /**
-    * Fetch the actions from database
-    *
-    * @param \Request $request              The Request object
-    * @param string   $contenttype          The slug of the contenttype
-    * @param int      $contenid             The id of the record
-    * @param int      $user_id              The id of the current user viewing the record
-    * @param int      $hoursToSubstract     The interval of hours to substract to query actions within the hours interval
-    *
-    * @return array The array of actions
-    */
+     * Fetch the actions from database
+     *
+     * @param Request $request The Request object
+     * @param string $contenttype The slug of the contenttype
+     * @param int $contentid The id of the record
+     * @param int $user_id The id of the current user viewing the record
+     * @param int $hoursToSubstract The interval of hours to substract to query actions within the hours interval
+     *
+     * @return array The array of actions
+     */
     public function fetchActions($request, $contenttype, $contentid, $user_id, $hoursToSubstract)
     {
 
-        if($request->get('_route') == 'editcontent') {
-
+        if ($request->get('_route') == 'editcontent') {
             if ($this->exist($contenttype, $contentid, $user_id)) {
                 $this->update($contenttype, $contentid, $user_id, 'editcontent');
             } else {
                 $this->insert($contenttype, $contentid, $user_id);
             }
-
-        }
-        elseif ($request->get('_route') == 'fileedit') {
-
         }
 
-        $actionsSelectSQL = "SELECT user.displayname, action.action, action.contenttype, action.record_id FROM bolt_users user, bolt_extension_who_is_editing action";
-        $actionsSelectSQL .= " WHERE action.user_id = user.id and action.action != 'close'";
+        $actionsSelectSQL = "SELECT user_table.displayname, action.action, action.contenttype, action.record_id FROM bolt_users user_table, bolt_extension_who_is_editing action";
+        $actionsSelectSQL .= " WHERE action.user_id = user_table.id and action.action != 'close'";
         $actionsSelectSQL .= " AND action.record_id = :record_id";
         $actionsSelectSQL .= " AND action.contenttype = :contenttype";
-        $actionsSelectSQL .= " AND action.date >= DATE_ADD('" . date("Y-m-d H:i:s") . "', INTERVAL -$hoursToSubstract HOUR)";
+        $actionsSelectSQL .= " AND action.date >= '".(new \DateTime())->modify('-'.$hoursToSubstract.' hours')->format('Y-m-d H:i:s')."'";
         $actionsSelectSQL .= " AND action.user_id != :action_user_id";
-        $actionsSelectSQL .= " AND user.id != :user_id";
+        $actionsSelectSQL .= " AND user_table.id != :user_id";
 
         $statement = $this->database->prepare($actionsSelectSQL);
         $statement->bindParam('record_id', $contentid);
@@ -87,8 +79,8 @@ class WhoIsEditingService {
      * Check if an Action record exist in the database
      *
      * @param string $contenttype The slug of the contenttype
-     * @param int    $contenid    The id of the record
-     * @param int    $user_id     The id of the current user viewing the record
+     * @param int $contenid The id of the record
+     * @param int $user_id The id of the current user viewing the record
      *
      * @return boolean
      */
@@ -107,8 +99,7 @@ class WhoIsEditingService {
 
         $selectQueryBuilderResults = $selectQueryBuilder->execute()->fetchAll();
 
-        if( empty($selectQueryBuilderResults) )
-        {
+        if (empty($selectQueryBuilderResults)) {
             return false;
         }
 
@@ -119,8 +110,8 @@ class WhoIsEditingService {
      * Insert a new Action record in the database
      *
      * @param string $contenttype The slug of the contenttype
-     * @param int    $contenid    The id of the record
-     * @param int    $user_id     The id of the current user viewing the record
+     * @param int $contenid The id of the record
+     * @param int $user_id The id of the current user viewing the record
      *
      * @return void
      */
@@ -132,18 +123,17 @@ class WhoIsEditingService {
                 'contenttype' => $contenttype,
                 'record_id' => $contentid,
                 'action' => 'editcontent',
-                'date' => date("Y-m-d H:i:s")
-            ])
-        ;
+                'date' => date("Y-m-d H:i:s"),
+            ]);
     }
 
     /**
      * Modify an Action record in the database
      *
      * @param string $contenttype The slug of the contenttype
-     * @param int    $contenid    The id of the record
-     * @param int    $user_id     The id of the current user viewing the record
-     * @param string $action      The action performed
+     * @param int $contenid The id of the record
+     * @param int $user_id The id of the current user viewing the record
+     * @param string $action The action performed
      *
      * @return void
      */
@@ -173,5 +163,4 @@ class WhoIsEditingService {
     {
         return $this->actionsMetaData;
     }
-
 }
